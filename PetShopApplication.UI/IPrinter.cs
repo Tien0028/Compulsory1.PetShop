8d7bf0e80086696b49faf0151eff7a077b289411
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-09-02 14:39:50 +0200

    Add files via upload

---
+++ PetShopApplication.UI/IPrinter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShopApplication.ConsoleApp
+{
+    public interface IPrinter
+    {
+        public void StartUI();
+    }
+}
